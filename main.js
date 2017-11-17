7c6a55291fbf22644e5e468899b90b571955ea04
Umoxfo <ycco34vx@gmail.com>
2017-11-16 22:46:48 -0800

    Add 'eslint' line

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define, brackets, $ */
+/*eslint-env node, es6, jquery */
 
 define(function (require, exports, module) {
     'use strict';
--- node/ValidationServer.js
+++ node/ValidationServer.js
@@ -1,5 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, node: true */
 /*global $, require */
+/*eslint-env node, es6, jquery */
 
 (function () {
     'use strict';
