9a2aaec0ef22cc348482ec1be8a2e73b422e3101
Umoxfo <ycco34vx@gmail.com>
2017-12-02 16:39:04 -0800

    Delete jquery statement

--- node/DependencyManager.js
+++ node/DependencyManager.js
@@ -1,6 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, node: true */
-/*global $, require, Promise */
-/*eslint-env node, es6, jquery */
+/*global require, Promise */
+/*eslint-env node, es6 */
 
 'use strict';
 
