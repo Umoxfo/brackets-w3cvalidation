40c5a1bde61ae001d7e1b72e74b411f51ceee6b7
Umoxfo <ycco34vx@gmail.com>
2018-03-28 09:47:38 -0700

    Improve build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
 'use strict';
 
-const gulp = require('gulp'),
-      del = require('del'),
-      exec = require('child_process').execSync,
-      uglify = require('gulp-uglify-es').default,
+/* eslint-disable indent, node/no-unpublished-require */
+const del = require('del'),
+      exec = require('child_process').exec,
+      gulp = require('gulp'),
       merge = require('event-stream').merge,
+      path = require('path'),
+      uglify = require('gulp-uglify-es').default,
       zip = require('gulp-zip');
-const pkgInfo = require('./package.json');
 
-gulp.task('clean', () => del(['build/']));
+const pkgInfo = require('./package.json'),
+      config = require('./gulpconf.json');
+/* eslint-enable indent, node/no-unpublished-require */
 
-gulp.task('resolve:node-deps', () => exec('cd ./node && npm update'));
+gulp.task('clean', () => del(['dist/**/*']));
+
+gulp.task('resolve:node-deps', () => exec('npm update', {cwd: path.join(__dirname, 'node')}));
 
 gulp.task('build', ['clean', 'resolve:node-deps'], () => {
-    let copy = gulp.src(['package.json', 'node/node_modules/**/*', 'node/dependency.json'], {base: '.'}).pipe(gulp.dest('build'));
-    let minify = gulp.src(['main.js', 'node/*.js'], {base: '.'})
-                     .pipe(uglify())
-                     .pipe(gulp.dest('build'));
+    const copy = gulp.src(config.resource.src, {base: '.'});
+    const minify = gulp.src(config.code.src, {base: '.'}).pipe(uglify({ecma: 6}));
 
-    return merge(copy, minify).on('end', () =>
-            gulp.src('build/**/*', {base: 'build'})
-                .pipe(zip(`${pkgInfo.name}-${pkgInfo.version}.zip`))
-                .pipe(gulp.dest('build/dist')));
+    return merge(copy, minify)
+        .pipe(zip(`${pkgInfo.name}-${pkgInfo.version}.zip`))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['build']);
