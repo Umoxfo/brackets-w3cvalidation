6d77befe2d63eff2894a006ced6ea7ab3fa2b7db
Umoxfo <ycco34vx@gmail.com>
2018-05-18 11:55:24 -0700

    Migrate to gulp v4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,38 @@
 'use strict';
 
 /* eslint-disable indent, node/no-unpublished-require */
-const del = require('del'),
-      exec = require('child_process').exec,
-      gulp = require('gulp'),
-//      merge = require('event-stream').merge,
+const exec = require('child_process').exec,
       path = require('path'),
-      uglify = require('gulp-uglify-es').default;
-//      zip = require('gulp-zip');
+      del = require('del'),
+      gulp = require('gulp'),
+      uglify = require('gulp-uglify-es').default,
+      merge2 = require('merge2'),
+      filter = require('gulp-filter'),
+      zip = require('gulp-vinyl-zip').zip;
 
-//const pkgInfo = require('./package.json'),
-//      config = require('./gulpconf.json');
-const config = require('./gulpconf.json');
+const pkgInfo = require('./package.json'),
+      config = require('./gulpconf.json'),
+      srcFilter = filter(config.code.src, {restore: true});
 /* eslint-enable indent, node/no-unpublished-require */
 
 gulp.task('clean', () => del(['dist/**/*']));
 
-gulp.task('resolve:node-deps', () => exec('npm update', {cwd: path.join(__dirname, 'node')}));
+gulp.task('update-deps', () => exec('npm update', {cwd: path.join(__dirname, 'node')}));
 
-gulp.task('copy', () => gulp.src(config.resource.src, {base: '.'}).pipe(gulp.dest('dist')));
-
-gulp.task('minify', () => {
-    return gulp.src(config.code.src, {base: '.'})
+gulp.task('compile', gulp.series(gulp.parallel('clean', 'update-deps'), () => {
+    return gulp.src(config.dist.src, {base: '.'})
+        .pipe(srcFilter)
         .pipe(uglify({ecma: 6}))
-        .pipe(gulp.dest('dist'));
-});
-
-/*
-gulp.task('build', gulp.series(gulp.parallel('clean', 'resolve:node-deps'), () => {
-    const copy = gulp.src(config.resource.src, {base: '.'});
-    const minify = gulp.src(config.code.src, {base: '.'}).pipe(uglify({ecma: 6}));
-
-    return merge(copy, minify)
+        .pipe(srcFilter.restore)
         .pipe(zip(`${pkgInfo.name}-${pkgInfo.version}.zip`))
         .pipe(gulp.dest('dist'));
 }));
-*/
 
-gulp.task('newbuild', gulp.series(gulp.parallel('clean', 'resolve:node-deps'), gulp.parallel('copy', 'minify')));
+gulp.task('build', gulp.series(gulp.parallel('clean', 'update-deps'), () => {
+    return merge2(gulp.src(config.code.src, {base: '.'}).pipe(uglify({ecma: 6})),
+                  gulp.src(config.resource.src, {base: '.'})) // eslint-disable-line indent
+        .pipe(zip(`${pkgInfo.name}-${pkgInfo.version}.zip`))
+        .pipe(gulp.dest('dist'));
+}));
 
 gulp.task('default', gulp.series('build'));
